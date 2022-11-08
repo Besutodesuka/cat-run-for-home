13f99d7a8c1c0ccf8aefe0d0204bfe157b869d75
Besutodesu <60954224+Besutodesuka@users.noreply.github.com>
2022-11-08 23:27:34 +0700

    feat: connect animation to playercontroller

--- cat fit escape dog/Assets/Scripts/PlayerControl_keyboard.cs
+++ cat fit escape dog/Assets/Scripts/PlayerControl_keyboard.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,17 +6,23 @@ using UnityEngine;
 public class PlayerControl_keyboard : MonoBehaviour
 {
     Rigidbody2D body;
+    public Animator animator;
 
-    bool isjumpped;
+    private bool isjumpped;
+    private bool isSlide;
+    private bool isRun;
     public int height;
+    private float speed;
 
     // Start is called before the first frame update
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
         isjumpped = false;
+        isSlide = false;
+        isRun = false;
+        speed = 0;
     }
-
     // Update is called once per frame
     void Update()
     {
@@ -23,17 +30,39 @@ public class PlayerControl_keyboard : MonoBehaviour
             // jump action
             body.velocity = new Vector3(0,height,0); // make player jump
             isjumpped = true;
-        } else if (Input.GetKey("down") && isjumpped ==false){
+            animator.SetBool("isjump",true);
+
+            isSlide = false;
+            animator.SetFloat("speed",speed);
+            isRun = false;
+        } 
+        else if (Input.GetKey("down")  && isjumpped == false){
             // Slide action
+            isSlide = true;
+            animator.SetBool("isslide",true);
             
+            isjumpped = false;
+            animator.SetFloat("speed",speed);
+            isRun = false;
+        }
+        else if ((Input.GetKey("right") == true) && (isjumpped == false)){
+            // Slide action
+            speed = Mathf.Min(1,speed+1);
+            animator.SetFloat("speed",speed);
         }
         else{
-            // Idle Action
+            speed = Mathf.Max(0,speed-1);
+            animator.SetFloat("speed",speed);
+            isSlide = false;
+            animator.SetBool("isslide",false);
         }
     }
 
     private void OnCollisionEnter2D(Collision2D col){
         isjumpped = false;
+        isSlide = false;
+        animator.SetBool("isjump",false);
+        animator.SetBool("isslide",false);
     }
 }
 
